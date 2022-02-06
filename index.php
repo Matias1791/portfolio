b9110a29964d0290d204f5b56e1d438e47586590
Matias1791 <moscatachado@hotmail.com>
2022-02-06 14:44:23 -0300

    cambios

--- index.php
+++ index.php
@@ -44,23 +44,7 @@ $pg = "index";
         </div>
     </main>
 
-    <footer class="container mt-5">
-        <div class="row">
-            <div class="col-12 col-sm-3 text-center text-sm-start">
-                <a href="https://github.com" target="_blank" title="Github"><i class="fab fa-github"></i></a>
-                <a href="https://www.linkedin.com" target="_blank" title="Linkedin"><i
-                        class="fab fa-linkedin-in"></i></a>
-            </div>
-            <div class="col-12 col-sm-3 text-center text-sm-start">Sponsor <a href="https://depcsuite.com/"
-                    target="_blank">DePC Suit</a></div>
-            <div class="col-12 col-sm-3 text-center text-sm-start"><a
-                    href="to:moscatachado@hotmail.com">moscatachado@hotmail.com</a></div>
-        </div>
-        <div class="bloque-whatsapp">
-            <a href="https://api.whatsapp.com/send?phone=541151163867"><i class="fab fa-whatsapp pb-4 pt-3 px-3"></i>
-            </a>
-        </div>
-    </footer>
+    <?php include_once "footer.php"; ?>
 
 </body>
 <script src="./css/bootstrap/js/bootstrap.bundle.min.js"></script>
